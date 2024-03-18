d8a8816e4fab1048f68fc11df36f91f31b81b6b2
tren <tren@parasoft.com>
2024-03-18 15:18:49 +0800

    add new report

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,13 +8,13 @@ class CalculatorTest {
     void testAdd() {
         Calculator calculator = new Calculator();
         int result = calculator.add(1, 2);
-        assertEquals(3, result);
+        assertEquals(1, result);
     }
 
     @Test
     void testMinus() {
         Calculator calculator = new Calculator();
         int result = calculator.minus(2, 1);
-        assertEquals(1, result);
+        assertEquals(2, result);
     }
 }
